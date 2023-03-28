05302625a3d64ef183493213d120106551a71675
NTAvanHoeffelen <niels.hoef@gmail.com>
2023-03-28 11:20:43 +0200

    changed parameter indicing

--- HSnet_wrapper.py
+++ HSnet_wrapper.py
@@ -4,10 +4,10 @@ import test as test
 import sys
 
 if __name__ == "__main__":
-    print(sys.argv[0])
-    if sys.argv[0] == ["test"]:
-        test.main(sys.argv[1:])
-    elif sys.argv[0] == ["train"]:
-        train.main(sys.argv[1:])
+    print(sys.argv[1])
+    if sys.argv[1] == "test":
+        test.main(sys.argv[2:])
+    elif sys.argv[1] == "train":
+        train.main(sys.argv[2:])
     else:
         print("only 'test' and 'train' are supported commands")
